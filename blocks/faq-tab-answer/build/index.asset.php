46d1473251d447871c6d77942d20a29d8ca1bb9e
Jasper Frumau <jasper@imagewize.com>
2025-12-24 14:25:48 +0700

    FAQ Demo Answers

--- blocks/faq-tab-answer/build/index.asset.php
+++ blocks/faq-tab-answer/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '77498ca4bcf99db9e9df');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '27f26456a5548c19a369');
--- blocks/faq-tabs/build/index.asset.php
+++ blocks/faq-tabs/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a306a59ec376e8e2280e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0d21b55aa0a62d178e80');

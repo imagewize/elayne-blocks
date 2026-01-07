66188ddde1ad41144957977e0368cd549fa7303e
Jasper Frumau <jasper@imagewize.com>
2026-01-07 15:52:12 +0700

    proper build

--- blocks/search-overlay-trigger/build/index.asset.php
+++ blocks/search-overlay-trigger/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '988e8588f1b748db5edb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '74c75ed9b9df186ff21c');

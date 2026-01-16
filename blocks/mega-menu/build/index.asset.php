30fa6f79bf3c50eee7119c14b23d34e32a13b783
Jasper Frumau <jasper@imagewize.com>
2026-01-16 18:13:22 +0700

    eturn <InnerBlocks.Content />;

--- blocks/mega-menu/build/index.asset.php
+++ blocks/mega-menu/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c43013b1f8496e0c0862');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1ba1bea5a0a17f383c42');
--- blocks/mega-menu/src/save.js
+++ blocks/mega-menu/src/save.js
@@ -1,12 +1,17 @@
 /**
- * Since this block uses server-side rendering via render.php,
- * the save function returns null to indicate that the block
- * should be rendered on the server.
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * For server-side rendering with InnerBlocks, we need to save the InnerBlocks
+ * content so it can be passed to render.php as $content.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
  *
- * @return {null} Returns null for server-side rendering.
+ * @return {Element} Element to render.
  */
+import { InnerBlocks } from '@wordpress/block-editor';
+
 export default function save() {
-	return null;
+	return <InnerBlocks.Content />;
 }

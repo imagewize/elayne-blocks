5e828ccbbd687d06a9d3cc89555c215642db6003
Jasper Frumau <jasper@imagewize.com>
2026-01-21 15:19:05 +0700

    Fix plugin header formatting and document pattern limitations
    This addresses the WordPress core limitation where user-created patterns
    (wp_block post type) cannot have blockTypes metadata and therefore cannot
    be filtered by template part area.

--- elayne-blocks.php
+++ elayne-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: Elayne Blocks
  * Plugin URI: https://github.com/imagewize/elayne-blocks
  * Description: Custom blocks for the Elayne WordPress theme including Mega Menu, Carousel, and Slide blocks
-2.5.8
+ * Version: 2.5.8
  * Requires at least: 6.7
  * Requires PHP: 7.3
  * Author: Jasper Frumau

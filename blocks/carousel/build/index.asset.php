66a485c9a35892e75b1343d151c179daa5aabe44
Jasper Frumau <jasper@imagewize.com>
2026-02-15 14:49:12 +0700

    Fix carousel block text domain consistency
    Update text domain from 'elayne' to 'elayne-blocks' in both source and build files for proper WordPress.org translation support.

--- blocks/carousel/build/index.asset.php
+++ blocks/carousel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ae881656c0b0a1957e4b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5a744c0b33b7cc913ad5');
